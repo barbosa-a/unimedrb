3e1b4df461f435e18b882111e7e5fc05e7df2980
Kalleb Alves <kallebalves@gmail.com>
2025-08-12 11:42:54 -0500

    Update config.php

--- gerenciar-site/config/config.php
+++ gerenciar-site/config/config.php
@@ -16,8 +16,8 @@ if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
 } else {
     $protocol = 'http';
 
-    define('pg', "$protocol://$url_host/gerenciar-site");
-    define('site', "$protocol://$url_host/");
+    define('pg', "$protocol://$url_host/$site/gerenciar-site");
+    define('site', "$protocol://$url_host/$site/");
 }
 
 define('nomeSistema', 'Área Restrita');
